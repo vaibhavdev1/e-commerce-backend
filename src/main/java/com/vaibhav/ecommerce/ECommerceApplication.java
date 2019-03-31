2182c199e959d3a4bb975d7a75a1cbd0dc729771
vaibhavdev1 <36061295+vaibhavdev1@users.noreply.github.com>
2019-03-31 20:44:28 +0530

    back-created_entry_point_class (#2)

--- src/main/java/com/vaibhav/ecommerce/ECommerceApplication.java
+++ src/main/java/com/vaibhav/ecommerce/ECommerceApplication.java
@@ -1,19 +1,12 @@
 package com.vaibhav.ecommerce;
 
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ECommerceApplication implements ApplicationRunner {
+public class ECommerceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ECommerceApplication.class, args);
-    }
-
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        System.out.println("Hello World");
+        System.exit(SpringApplication.exit(SpringApplication.run(ECommerceApplication.class, args)));
     }
 }
---
+++ src/main/java/com/vaibhav/ecommerce/EntryPoint/EntryPointBean.java
@@ -0,0 +1,14 @@
+package com.vaibhav.ecommerce.EntryPoint;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EntryPointBean implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("Hello World");
+    }
+}
